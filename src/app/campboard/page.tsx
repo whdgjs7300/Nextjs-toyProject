676ca12ee54b91985d16e810b15afe3b3450c336
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-08-26 23:50:27 +0900

    8/26 useRouter 클라이언트 모듈 해결

--- src/app/campboard/page.tsx
+++ src/app/campboard/page.tsx
@@ -1,8 +1,11 @@
-
+'use client'
+import { usePathname, useRouter } from 'next/navigation';
 
 export default  function CampBoard() {
+    const router = useRouter(); 
+    const pathname = usePathname();
 
-
+    console.log(router,pathname)
     return (
         <div>
             
