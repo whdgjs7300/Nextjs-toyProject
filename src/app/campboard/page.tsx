92240b4ebdb788afca9aaa1ac90fd7caf3a13f74
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-08-27 23:00:25 +0900

    8/27 해당페이지 데이터 셋

--- src/app/campboard/page.tsx
+++ src/app/campboard/page.tsx
@@ -1,11 +1,23 @@
 'use client'
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter,useSearchParams} from 'next/navigation';
 
 export default  function CampBoard() {
-    const router = useRouter(); 
-    const pathname = usePathname();
+    const locationData = sessionStorage.getItem('location');
 
-    console.log(router,pathname)
+    // locationData 값이 있는 경우에만 파싱하여 lat와 lon 값을 가져오기
+    let lat = null;
+    let lon = null;
+    if (locationData) {
+        const { lat: storedLat, lon: storedLon } = JSON.parse(locationData);
+        lat = storedLat;
+        lon = storedLon;
+    }
+
+    console.log('Lat:', lat);
+    console.log('Lon:', lon);
+
+    
+
     return (
         <div>
             
