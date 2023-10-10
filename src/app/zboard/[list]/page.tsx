54408c17c121e6e964ffa3dfc28b384dcfb94985
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-10-10 18:32:49 +0900

    10/10 zboard 조건부렌더링 및 board 타입구성, 문법구성

--- src/app/zboard/[list]/page.tsx
+++ src/app/zboard/[list]/page.tsx
@@ -6,13 +6,15 @@ import {useSearchParams} from 'next/navigation';
 import Board from '@/components/Board';
 
 export default function Zboard() {
+    // code별로 렌더되는 컴포넌트 내용 다르게 짜야함
+    
     const searchParams = useSearchParams();
     const code = searchParams.get('code');
     console.log(code)
 
     return (
         <div >
-            
+            <Board/>
         </div>
     )
     
