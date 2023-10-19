b867ee8db98fa83ca31ed223bd8e2352f76ed142
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-10-19 22:37:15 +0900

    10/19 맵 페이지 css 셋 , 카드 클릭시 해당지도 위치, 마커 이동

--- src/components/MapCard.tsx
+++ src/components/MapCard.tsx
@@ -1,19 +1,23 @@
-import styles from '../CSS/Detail.module.css';
+
+import styles from '../CSS/Map.module.css';
+
 
 type DataProps = {
     item? : LocationCampingData,
-    currentPage? : number,
+    onCardClick? : (item: LocationCampingData) => void
 } 
 
 
-export default function MapCard({item}: DataProps) {
+export default function MapCard({item, onCardClick}: DataProps) {
 
     
 return (
-        <div >
+        <div 
+        className={styles.mapcard_info}>
             <h3>[{item?.doNm} {item?.sigunguNm} {item?.facltNm}]</h3>
             <p>{item?.addr1 || item?.addr2}</p>
             <p>{item?.tel}</p>
+            
         </div>
 
 
